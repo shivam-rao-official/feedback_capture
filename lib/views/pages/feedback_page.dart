adce3e3bc08f2bd7579b1426123249abdc0c59b4
arpan2580 <adas25150@gmail.com>
2022-03-29 18:46:01 +0530

    feedback page full design

--- lib/views/pages/feedback_page.dart
+++ lib/views/pages/feedback_page.dart
@@ -1,8 +1,13 @@
 import 'package:feedback_capture/consts/app_fonts.dart';
 import 'package:feedback_capture/consts/app_sizes.dart';
 import 'package:feedback_capture/consts/app_themes.dart';
+import 'package:feedback_capture/controllers/category_controller.dart';
+import 'package:feedback_capture/controllers/company_controller.dart';
 import 'package:feedback_capture/controllers/feedback_type_controller.dart';
 import 'package:feedback_capture/controllers/outlet_controller.dart';
+import 'package:feedback_capture/controllers/sub_category_controller.dart';
+import 'package:feedback_capture/models/category.dart';
+import 'package:feedback_capture/models/company.dart';
 import 'package:feedback_capture/models/feedback_type.dart';
 import 'package:feedback_capture/models/outlet.dart';
 import 'package:feedback_capture/views/widgets/custom_button.dart';
@@ -18,13 +23,34 @@ class FeedbackPage extends StatefulWidget {
 
 class _FeedbackPageState extends State<FeedbackPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String? selectedOutletValue, selectedFeedbackValue;
+  String? selectedOutletValue,
+      selectedFeedbackValue,
+      selectedCompanyValue,
+      selectedCategoryValue,
+      selectedSubCategoryValue,
+      selectedGenreOfFeedback;
 
   final _outletController = OutletController();
   final _feedbackController = FeedbackTypeController();
+  final _companyController = CompanyController();
+  final _categoryController = CategoryController();
+  final _subCategoryController = SubCategoryController();
 
   List<Outlet> outletItems = [];
   List<FeedbackType> feedbackItems = [];
+  List<Company> companyItems = [];
+  List<Category> categoryItems = [];
+  List subCategoryItems = [];
+  List genreOfFeedback = [
+    "Product",
+    "Price",
+    "Packaging",
+    "Product Availability",
+    "Merchandising solutions",
+    "Others"
+  ];
+
+  late String _feedback;
 
   @override
   void initState() {
@@ -39,6 +65,11 @@ class _FeedbackPageState extends State<FeedbackPage> {
         outletItems = _outletController.outlets;
         _feedbackController.getFeedbackTypes();
         feedbackItems = _feedbackController.feedbackTypes;
+        _companyController.getCompanies();
+        companyItems = _companyController.companyTypes;
+        _categoryController.getCategories();
+        categoryItems = _categoryController.categoryTypes;
+        _subCategoryController.getSubCategories();
       });
     }
   }
@@ -47,6 +78,9 @@ class _FeedbackPageState extends State<FeedbackPage> {
   void dispose() {
     _outletController.dispose();
     _feedbackController.dispose();
+    _companyController.dispose();
+    _categoryController.dispose();
+    _subCategoryController.dispose();
     super.dispose();
   }
 
@@ -180,6 +214,151 @@ class _FeedbackPageState extends State<FeedbackPage> {
                                       value: value.feedbackName,
                                     );
                                   }).toList(),
+                                ),
+                                const SizedBox(
+                                  height: 7.0,
+                                ),
+                                DropdownButtonFormField(
+                                  decoration: const InputDecoration(
+                                    hintText: "Select Company",
+                                  ),
+                                  style: const TextStyle(
+                                    fontFamily: AppFonts.appFont,
+                                    fontSize: AppFonts.fontSize,
+                                    color: Colors.black87,
+                                  ),
+                                  validator: (value) => value == null
+                                      ? "Select a company type"
+                                      : null,
+                                  value: selectedCompanyValue,
+                                  onChanged: (String? newValue) {
+                                    setState(() {
+                                      selectedCompanyValue = newValue!;
+                                    });
+                                  },
+                                  items: companyItems
+                                      .map<DropdownMenuItem<String>>(
+                                          (Company value) {
+                                    return DropdownMenuItem(
+                                      child: Text(value.companyName),
+                                      value: value.companyName,
+                                    );
+                                  }).toList(),
+                                ),
+                                const SizedBox(
+                                  height: 7.0,
+                                ),
+                                DropdownButtonFormField(
+                                  decoration: const InputDecoration(
+                                    hintText: "Select Category",
+                                  ),
+                                  style: const TextStyle(
+                                    fontFamily: AppFonts.appFont,
+                                    fontSize: AppFonts.fontSize,
+                                    color: Colors.black87,
+                                  ),
+                                  validator: (value) => value == null
+                                      ? "Select a category"
+                                      : null,
+                                  value: selectedCategoryValue,
+                                  onChanged: (String? newValue) {
+                                    setState(() {
+                                      selectedCategoryValue = newValue!;
+                                      int index = categoryItems.indexWhere(
+                                          (element) =>
+                                              element.categoryName ==
+                                              selectedCategoryValue);
+
+                                      subCategoryItems = _subCategoryController
+                                          .subCategoryTypes[index]
+                                          .subCategoryName;
+
+                                      selectedSubCategoryValue =
+                                          subCategoryItems[0];
+                                    });
+                                  },
+                                  items: categoryItems
+                                      .map<DropdownMenuItem<String>>(
+                                          (Category value) {
+                                    return DropdownMenuItem(
+                                      child: Text(value.categoryName),
+                                      value: value.categoryName,
+                                    );
+                                  }).toList(),
+                                ),
+                                const SizedBox(
+                                  height: 7.0,
+                                ),
+                                DropdownButtonFormField(
+                                  isExpanded: true,
+                                  decoration: const InputDecoration(
+                                    hintText: "Select Sub Category",
+                                  ),
+                                  style: const TextStyle(
+                                    fontFamily: AppFonts.appFont,
+                                    fontSize: AppFonts.fontSize,
+                                    color: Colors.black87,
+                                  ),
+                                  validator: (value) => value == null
+                                      ? "Select a sub category"
+                                      : null,
+                                  value: selectedSubCategoryValue,
+                                  onChanged: (String? newValue) {
+                                    setState(() {
+                                      selectedSubCategoryValue = newValue!;
+                                    });
+                                  },
+                                  items: subCategoryItems
+                                      .map<DropdownMenuItem<String>>((subCat) {
+                                    return DropdownMenuItem(
+                                      child: Text(subCat.toString()),
+                                      value: subCat.toString(),
+                                    );
+                                  }).toList(),
+                                ),
+                                const SizedBox(
+                                  height: 7.0,
+                                ),
+                                DropdownButtonFormField(
+                                  isExpanded: true,
+                                  decoration: const InputDecoration(
+                                    hintText: "Select genre of feedback",
+                                  ),
+                                  style: const TextStyle(
+                                    fontFamily: AppFonts.appFont,
+                                    fontSize: AppFonts.fontSize,
+                                    color: Colors.black87,
+                                  ),
+                                  validator: (value) => value == null
+                                      ? "Select a genre of feedback"
+                                      : null,
+                                  value: selectedGenreOfFeedback,
+                                  onChanged: (String? newValue) {
+                                    setState(() {
+                                      selectedGenreOfFeedback = newValue!;
+                                    });
+                                  },
+                                  items: genreOfFeedback
+                                      .map<DropdownMenuItem<String>>(
+                                          (feedback) {
+                                    return DropdownMenuItem(
+                                      child: Text(feedback.toString()),
+                                      value: feedback.toString(),
+                                    );
+                                  }).toList(),
+                                ),
+                                const SizedBox(
+                                  height: 7.0,
+                                ),
+                                TextFormField(
+                                  minLines: 4,
+                                  keyboardType: TextInputType.multiline,
+                                  textInputAction: TextInputAction.newline,
+                                  maxLines: 4,
+                                  decoration: const InputDecoration(
+                                    hintText: "Enter your feedback",
+                                  ),
+                                  onSaved: (value) => _feedback = value!,
                                 ),
                                 const SizedBox(
                                   height: 30,
